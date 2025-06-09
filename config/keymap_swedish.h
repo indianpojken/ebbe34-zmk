3551c89a6c8e5ee85ac6794fe9fcebae1c828c94
indianpojken <io.qrdr@gmail.com>
2025-06-10 01:42:15 +0200

    wip config

---
+++ config/keymap_swedish.h
@@ -0,0 +1,45 @@
+// Source file: https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_swedish.h
+
+#define SE_SECT    GRAVE        // §
+#define SE_PLUS    MINUS        // +
+#define SE_ACUT    EQUAL        // ´ (dead)
+#define SE_ARNG    LBKT         // Å
+#define SE_DIAE    RBKT         // ¨ (dead)
+#define SE_ODIA    SEMI         // Ö
+#define SE_ADIA    SQT          // Ä
+#define SE_QUOT    NUHS         // '
+#define SE_LABK    NUBS         // <
+#define SE_COMM    COMMA        // ,
+#define SE_DOT     DOT          // .
+#define SE_MINS    SLASH        // -
+#define SE_HALF    LS(SE_SECT)        // ½
+#define SE_EXLM    LS(N1)           // !
+#define SE_DQUO    LS(N2)           // "
+#define SE_HASH    LS(N3)           // #
+#define SE_CURR    LS(N4)           // ¤
+#define SE_PERC    LS(N5)           // %
+#define SE_AMPR    LS(N6)           // &
+#define SE_SLSH    LS(N7)           // /
+#define SE_LPRN    LS(N8)           // (
+#define SE_RPRN    LS(N9)           // )
+#define SE_EQL     LS(N0)           // =
+#define SE_QUES    LS(MINUS)        // ?
+#define SE_GRV     LS(EQUAL)        // ` (dead)
+#define SE_CIRC    LS(RBKT)         // ^ (dead)
+#define SE_ASTR    LS(NUHS)         // *
+#define SE_RABK    LS(NUBS)         // >
+#define SE_SCLN    LS(COMMA)        // ;
+#define SE_COLN    LS(DOT)          // :
+#define SE_UNDS    LS(SLASH)        // _
+#define SE_AT      RA(N2)     // @
+#define SE_PND     RA(N3)     // £
+#define SE_DLR     RA(N4)     // $
+#define SE_EURO    RA(N5)     // €
+#define SE_LCBR    RA(N7)     // {
+#define SE_LBRC    RA(N8)     // [
+#define SE_RBRC    RA(N9)     // ]
+#define SE_RCBR    RA(N0)     // }
+#define SE_BSLS    RA(MINUS)     // (backslash)
+#define SE_TILD    RA(RBKT)     // ~ (dead)
+#define SE_PIPE    RA(NUBS)     // |
+#define SE_MICR    RA(M)
