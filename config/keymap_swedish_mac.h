bebc0a2d93c663a2c0d00c0249622c579ccbac31
indianpojken <io.qrdr@gmail.com>
2025-06-22 23:49:47 +0200

    mac

---
+++ config/keymap_swedish_mac.h
@@ -0,0 +1,47 @@
+// Copyright 2025 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// Aliases
+#define SE_SECT GRAVE  // §
+#define SE_PLUS MINUS // +
+#define SE_ACUT EQUAL  // ´ (dead)
+#define SE_ARNG LBKT // Å
+#define SE_DIAE RBKT // ¨ (dead)
+#define SE_ODIA SEMI // Ö
+#define SE_ADIA SQT // Ä
+#define SE_QUOT NUHS // '
+#define SE_LABK LESS_THAN // <
+#define SE_COMM COMMA // ,
+#define SE_DOT  DOT  // .
+#define SE_MINS SLASH // -
+#define SE_HALF LS(SE_SECT)        // ½
+#define SE_EXLM LS(N1)    // !
+#define SE_DQUO LS(N2)    // "
+#define SE_HASH LS(N3)    // #
+#define SE_EURO LS(N4)    // €
+#define SE_PERC LS(N5)    // %
+#define SE_AMPR LS(N6)    // &
+#define SE_SLSH LS(N7)    // /
+#define SE_LPRN LS(N8)    // (
+#define SE_RPRN LS(N9)    // )
+#define SE_EQL  LS(N0)    // =
+#define SE_QUES LS(SE_PLUS) // ?
+#define SE_GRV  LS(SE_ACUT) // `
+#define SE_CIRC LS(SE_DIAE) // ^ (dead)
+#define SE_ASTR LS(SE_QUOT) // *
+#define SE_RABK GREATER_THAN // >
+#define SE_SCLN LS(SE_COMM) // ;
+#define SE_COLN LS(SE_DOT)  // :
+#define SE_UNDS LS(SE_MINS) // _
+#define SE_PILC LA(SE_SECT) // ¶
+#define SE_AT   LA(N2)    // @
+#define SE_PND  LA(N3)    // £
+#define SE_DLR  LA(N4)    // $
+#define SE_PIPE LA(N7)    // |
+#define SE_LBRC LA(N8)    // [
+#define SE_RBRC LA(N9)    // ]
+#define SE_TILD LA(SE_DIAE) // ~ (dead)
+#define SE_BSLS LS(LA(N7))    // (backslash)
+#define SE_LCBR LS(LA(N8))    // {
+#define SE_RCBR LS(LA(N9))    // }
+
